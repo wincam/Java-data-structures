f5778cced37bdccf4d6fe936609139000838a3f0
Cameron <Cameron>
2016-02-05 22:06:56 -0500

    Implemented valued trees except search

--- src/datastructures/Tester.java
+++ src/datastructures/Tester.java
@@ -22,6 +22,27 @@ public class Tester{
 		linkedList.addList(linkedList2, 2);
 		System.out.println(linkedList.toString());
 		System.out.println(linkedList.getSubSection(3, 10).toString());
+		
+		
+		
+		System.out.println("Trees");
+		
+		ValuedTree<Integer> testTree = new ValuedTree<Integer>(10);
+		for (int i = 0; i < 7; i++){
+			testTree.getRoot().addChild(i*20,i);
+			for (int j = 0; j < 20; j++) {
+				testTree.getRoot().getChild(i).addChild(j*30,j);
+			}
+		}
+		
+		System.out.println(testTree.nodeExist(new int[]{7}));
+		
+		System.out.println(testTree.toString());
+		ValuedTreeNode<Integer> node = new ValuedTreeNode<>(300);
+		node.addChild(40, 0);
+		node.addChild(41, 0);
+		testTree.graft(node, new int[]{0,0,0});
+		System.out.println(testTree.toString());
 	}
 
 }
