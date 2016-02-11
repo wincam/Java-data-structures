e93d34b65d82f0fb92ad7a44e990c1da3cba95e9
Cameron <Cameron>
2016-02-10 19:24:34 -0500

    Implemented Valueless tree

--- src/datastructures/ValuelessTree.java
+++ src/datastructures/ValuelessTree.java
@@ -1,5 +1,12 @@
 package datastructures;
 
+/**
+ * A tree structure that does not hold any values
+ * 
+ * @author Cameron Nicolle
+ * @version 1.0
+ * @since 2/10/2016
+ */
 public class ValuelessTree implements Tree{
 	private ValuelessTreeNode root;
 	
@@ -44,14 +51,30 @@ public class ValuelessTree implements Tree{
 
 	@Override
 	public void prun(int[] index) {
-		// TODO Auto-generated method stub
-		
+		ValuelessTreeNode prunNode = this.getNode(index);
+		prunNode.getParent().removeChild(index[index.length - 1]);
 	}
 
 	@Override
 	public int getNodeChildCount(int[] index) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getNode(index).getNumChildren();
+	}
+	
+	/**
+	 * Add node and all it's children to the tree
+	 * 
+	 * @param node	The node to add
+	 * @param index	An array of integers that represents the index where the node must be inserted
+	 */
+	public void graft(ValuelessTreeNode node, int[] index) {
+		//create the index of the intended parent node
+		int[] parentIndex = new int[index.length - 1];
+		for (int i = 0; i < parentIndex.length; i++){
+			parentIndex[i] = index[i];
+		}
+		
+		ValuelessTreeNode parent = this.getNode(parentIndex);
+		parent.addChildNode(index[index.length - 1], node);
 	}
 
 }
